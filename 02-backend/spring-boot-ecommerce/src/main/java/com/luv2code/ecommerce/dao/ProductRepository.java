3324a3c2f744c05fb77156f4065a2213c5a4c808
Gustave Ekobe <ezekieleko80@gmail.comclear>
2024-02-09 18:32:38 -0600

    Angular code change

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,22 @@
+package com.luv2code.ecommerce.dao;
+
+
+import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhost:4200")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    //added query method:
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    /**Behind the scene
+     * findByCategoryId = SELECT * FROM product WHERE category_id = ?*/
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
+}
+
+
